9e0127732cf112c7e5c17ace1f792efd187c03fc
pavils.kuzmins <pavils.kuzmins@gmail.com>
2023-04-12 15:47:12 +0300

    Add Owners index page and controller. Closes #12

---
+++ pet-clinic-web/src/main/java/pavils/sfgpetclinic/controllers/OwnersController.java
@@ -0,0 +1,13 @@
+package pavils.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping({"/owners"})
+@Controller
+public class OwnersController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
