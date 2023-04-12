ddfd2abb7bd73093a7b4a4e3a56484c7cfa7f634
pavils.kuzmins <pavils.kuzmins@gmail.com>
2023-04-12 13:37:06 +0300

    Implement map based services. Closes #5

---
+++ pet-clinic-data/src/main/java/pavils/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package pavils.springframework.sfgpetclinic.services.map;
+
+import pavils.springframework.sfgpetclinic.model.Owner;
+import pavils.springframework.sfgpetclinic.model.Pet;
+import pavils.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
