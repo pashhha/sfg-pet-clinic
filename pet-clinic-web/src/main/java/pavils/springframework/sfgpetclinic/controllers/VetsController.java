8d20201389e4b5b04c39f56eb64bceb8ea32ebdb
pavils.kuzmins <pavils.kuzmins@gmail.com>
2023-05-30 22:29:19 +0300

    Added list of vets. Closes #14

--- pet-clinic-web/src/main/java/pavils/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/pavils/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,12 +1,24 @@
 package pavils.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pavils.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetsController {
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+
+    VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
