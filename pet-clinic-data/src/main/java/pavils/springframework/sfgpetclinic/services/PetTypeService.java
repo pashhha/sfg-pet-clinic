645b2ed952524c7ac4f148240c52b0d3f9373046
pashhha <pavils.kuzmins@gmail.com>
2023-11-11 22:18:01 +0200

    added pet type services. Closes #28

---
+++ pet-clinic-data/src/main/java/pavils/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pavils.springframework.sfgpetclinic.services;
+
+import pavils.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
