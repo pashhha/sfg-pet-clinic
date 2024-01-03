805e8fffd79f379590cd004424466040711767a8
pashhha <pavils.kuzmins@gmail.com>
2024-01-03 20:44:32 +0200

    added pet type services data loader. Closes #31

--- pet-clinic-web/src/main/java/pavils/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pavils/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -3,7 +3,9 @@ package pavils.springframework.sfgpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pavils.springframework.sfgpetclinic.model.Owner;
+import pavils.springframework.sfgpetclinic.model.PetType;
 import pavils.springframework.sfgpetclinic.model.Vet;
+import pavils.springframework.sfgpetclinic.services.PetTypeService;
 import pavils.springframework.sfgpetclinic.services.map.OwnerServiceMap;
 import pavils.springframework.sfgpetclinic.services.map.VetServiceMap;
 
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerServiceMap ownerServiceMap;
     private final VetServiceMap vetServiceMap;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerServiceMap ownerServiceMap, VetServiceMap vetServiceMap) {
+    public DataLoader(OwnerServiceMap ownerServiceMap, VetServiceMap vetServiceMap, PetTypeService petTypeService) {
         this.ownerServiceMap = ownerServiceMap;
         this.vetServiceMap = vetServiceMap;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
          Owner owner1 = new Owner();
          owner1.setFirstName("Pavils");
