ac5ab035cb5f79cb908f51f86f9b00b0ab775289
pashhha <pavils.kuzmins@gmail.com>
2024-01-06 22:34:50 +0200

    Added specialities map impl. Closes #30

---
+++ pet-clinic-data/src/main/java/pavils/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package pavils.springframework.sfgpetclinic.services;
+
+import pavils.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
